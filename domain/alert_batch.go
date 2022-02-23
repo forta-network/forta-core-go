2f1508209f7d87d79811a1bd9348a856f973159e
Steven Landers <steven.landers@gmail.com>
2022-02-23 15:34:34 -0500

    add library code

---
+++ domain/alert_batch.go
@@ -0,0 +1,14 @@
+package domain
+
+import "forta-protocol/forta-core-go/protocol"
+
+type AlertBatch struct {
+	Scanner     string                     `json:"scanner"`
+	ChainID     int64                      `json:"chainId"`
+	BlockStart  int64                      `json:"blockStart"`
+	BlockEnd    int64                      `json:"blockEnd"`
+	AlertCount  int64                      `json:"alertCount"`
+	MaxSeverity int64                      `json:"maxSeverity"`
+	Ref         string                     `json:"ref"`
+	SignedBatch *protocol.SignedAlertBatch `json:"signedBatch"`
+}
