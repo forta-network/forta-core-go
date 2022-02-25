5b8a2f472abf7f9979dc96690efe139654201684
Steven Landers <steven.landers@gmail.com>
2022-02-25 16:46:36 -0500

    add time lib

--- utils/time.go
+++ utils/time.go
@@ -12,3 +12,15 @@ func ParseTime(s string) time.Time {
 	t, _ := time.Parse(time.RFC3339, s)
 	return t
 }
+
+//UnixToTime converts a epoch timestamp to a time object
+func UnixToTime(ms uint64) time.Time {
+	return time.Unix(0, int64(ms)*int64(time.Millisecond))
+}
+
+const invertTsNum = 10000000000
+
+// InvertTimestamp makes large numbers smaller.
+func InvertTimestamp(ts int64) int64 {
+	return invertTsNum - ts
+}
