844f89432a17f9920647fdbf3e83c348c5b4edf3
Steven Landers <steven.landers@gmail.com>
2022-02-25 16:53:07 -0500

    add missing methods

--- utils/types.go
+++ utils/types.go
@@ -12,3 +12,21 @@ func String(str *string) string {
 func StringPtr(str string) *string {
 	return &str
 }
+
+// Int32Ptr returns a pointer.
+func Int32Ptr(n int32) *int32 {
+	return &n
+}
+
+// BoolPtr returns a pointer.
+func BoolPtr(v bool) *bool {
+	return &v
+}
+
+// Bool returns a value.
+func Bool(v *bool) bool {
+	if v != nil {
+		return *v
+	}
+	return false
+}
