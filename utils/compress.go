d2398fb02162bfe1161d2b6332740dc8958adb81
Steven Landers <steven.landers@gmail.com>
2022-02-25 16:50:56 -0500

    add compress library

---
+++ utils/compress.go
@@ -0,0 +1,26 @@
+package utils
+
+import (
+	"bytes"
+	"compress/gzip"
+	"io"
+)
+
+// GzipEncode compresses bytes.
+func GzipEncode(b []byte) ([]byte, error) {
+	var buf bytes.Buffer
+	w := gzip.NewWriter(&buf)
+	w.Write(b)
+	w.Close()
+	return buf.Bytes(), nil
+}
+
+// GzipDecode decompresses bytes.
+func GzipDecode(b []byte) ([]byte, error) {
+	r, err := gzip.NewReader(bytes.NewBuffer(b))
+	if err != nil {
+		return nil, err
+	}
+	defer r.Close()
+	return io.ReadAll(r)
+}
