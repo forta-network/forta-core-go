b2cba6040cd52789c8e5b8b0891a965c7ea9f20a
Steven Landers <steven.landers@gmail.com>
2022-03-25 08:59:57 -0400

    add signed batch summary to batch payload

--- domain/alert_batch.go
+++ domain/alert_batch.go
@@ -3,12 +3,13 @@ package domain
 import "github.com/forta-protocol/forta-core-go/protocol"
 
 type AlertBatch struct {
-	Scanner     string                  `json:"scanner"`
-	ChainID     int64                   `json:"chainId"`
-	BlockStart  int64                   `json:"blockStart"`
-	BlockEnd    int64                   `json:"blockEnd"`
-	AlertCount  int64                   `json:"alertCount"`
-	MaxSeverity int64                   `json:"maxSeverity"`
-	Ref         string                  `json:"ref"`
-	SignedBatch *protocol.SignedPayload `json:"signedBatch"`
+	Scanner            string                  `json:"scanner"`
+	ChainID            int64                   `json:"chainId"`
+	BlockStart         int64                   `json:"blockStart"`
+	BlockEnd           int64                   `json:"blockEnd"`
+	AlertCount         int64                   `json:"alertCount"`
+	MaxSeverity        int64                   `json:"maxSeverity"`
+	Ref                string                  `json:"ref"`
+	SignedBatch        *protocol.SignedPayload `json:"signedBatch"`
+	SignedBatchSummary *protocol.SignedPayload `json:"batchSummary"`
 }
