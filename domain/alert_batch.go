39909d42eb4fd641287ad47f16f90cf54e60103f
Steven Landers <steven.landers@gmail.com>
2022-03-25 16:46:22 -0400

    add alert batch response

--- domain/alert_batch.go
+++ domain/alert_batch.go
@@ -4,7 +4,8 @@ import (
 	"github.com/forta-protocol/forta-core-go/protocol"
 )
 
-type AlertBatch struct {
+// AlertBatchRequest is the request payload to the alerts API
+type AlertBatchRequest struct {
 	Scanner            string                  `json:"scanner"`
 	ChainID            int64                   `json:"chainId"`
 	BlockStart         int64                   `json:"blockStart"`
@@ -14,4 +15,21 @@ type AlertBatch struct {
 	Ref                string                  `json:"ref"`
 	SignedBatch        *protocol.SignedPayload `json:"signedBatch"`
 	SignedBatchSummary *protocol.SignedPayload `json:"signedBatchSummary"`
+}
+
+// AlertBatchResponse is the response from the alerts API
+type AlertBatchResponse struct {
+	SignedReceipt *protocol.SignedPayload `json:"signedReceipt"`
+}
+
+// AlertBatchMessage is passed to batch processing asynchronously
+type AlertBatchMessage struct {
+	Scanner       string                  `json:"scanner"`
+	ChainID       int64                   `json:"chainId"`
+	BlockStart    int64                   `json:"blockStart"`
+	BlockEnd      int64                   `json:"blockEnd"`
+	AlertCount    int64                   `json:"alertCount"`
+	MaxSeverity   int64                   `json:"maxSeverity"`
+	Ref           string                  `json:"ref"`
+	SignedReceipt *protocol.SignedPayload `json:"signedReceipt"`
 }
