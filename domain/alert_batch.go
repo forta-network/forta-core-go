351eae7717f03f349ec74534b460c61ef4a62df8
Steven Landers <steven.landers@gmail.com>
2022-03-25 09:03:44 -0400

    add helper methods for getting decoded values

--- domain/alert_batch.go
+++ domain/alert_batch.go
@@ -1,6 +1,10 @@
 package domain
 
-import "github.com/forta-protocol/forta-core-go/protocol"
+import (
+	"github.com/forta-protocol/forta-core-go/encoding"
+	"github.com/forta-protocol/forta-core-go/protocol"
+	"github.com/forta-protocol/forta-core-go/security"
+)
 
 type AlertBatch struct {
 	Scanner            string                  `json:"scanner"`
@@ -12,4 +16,26 @@ type AlertBatch struct {
 	Ref                string                  `json:"ref"`
 	SignedBatch        *protocol.SignedPayload `json:"signedBatch"`
 	SignedBatchSummary *protocol.SignedPayload `json:"batchSummary"`
+}
+
+func (a *AlertBatch) Batch() (*protocol.AlertBatch, error) {
+	if err := security.VerifySignedPayload(a.SignedBatch); err != nil {
+		return nil, err
+	}
+	var res protocol.AlertBatch
+	if err := encoding.DecodeGzippedProto(a.SignedBatch.Encoded, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+func (a *AlertBatch) BatchSummary() (*protocol.BatchSummary, error) {
+	if err := security.VerifySignedPayload(a.SignedBatchSummary); err != nil {
+		return nil, err
+	}
+	var res protocol.BatchSummary
+	if err := encoding.DecodeGzippedProto(a.SignedBatchSummary.Encoded, &res); err != nil {
+		return nil, err
+	}
+	return &res, nil
 }
