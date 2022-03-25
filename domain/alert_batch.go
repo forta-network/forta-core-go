4f43bb992e7955242c2522e88d7b387c6b0edc7c
Steven Landers <steven.landers@gmail.com>
2022-03-25 09:15:35 -0400

    afraid of circular dependency garbage, removing

--- domain/alert_batch.go
+++ domain/alert_batch.go
@@ -1,10 +1,7 @@
 package domain
 
 import (
-	"github.com/forta-protocol/forta-core-go/encoding"
 	"github.com/forta-protocol/forta-core-go/protocol"
-	"github.com/forta-protocol/forta-core-go/security"
-	"github.com/golang/protobuf/proto"
 )
 
 type AlertBatch struct {
@@ -17,27 +14,4 @@ type AlertBatch struct {
 	Ref                string                  `json:"ref"`
 	SignedBatch        *protocol.SignedPayload `json:"signedBatch"`
 	SignedBatchSummary *protocol.SignedPayload `json:"batchSummary"`
-}
-
-func verifyAndDecode(p *protocol.SignedPayload, target proto.Message) error {
-	if err := security.VerifySignedPayload(p); err != nil {
-		return err
-	}
-	return encoding.DecodeGzippedProto(p.Encoded, target)
-}
-
-func (a *AlertBatch) Batch() (*protocol.AlertBatch, error) {
-	var res protocol.AlertBatch
-	if err := verifyAndDecode(a.SignedBatch, &res); err != nil {
-		return nil, err
-	}
-	return &res, nil
-}
-
-func (a *AlertBatch) BatchSummary() (*protocol.BatchSummary, error) {
-	var res protocol.BatchSummary
-	if err := verifyAndDecode(a.SignedBatchSummary, &res); err != nil {
-		return nil, err
-	}
-	return &res, nil
 }
