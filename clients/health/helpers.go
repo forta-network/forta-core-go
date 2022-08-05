c232854412ac0296cb0af0640325a522c3f56872
Caner Çıdam <canercidam01@gmail.com>
2022-08-05 19:48:56 +0300

    add number tracker to health package

--- clients/health/helpers.go
+++ clients/health/helpers.go
@@ -1,6 +1,7 @@
 package health
 
 import (
+	"strconv"
 	"sync"
 	"time"
 )
@@ -114,5 +115,29 @@ func (mt *MessageTracker) GetReport(name string) *Report {
 	report.Name = name
 	report.Status = StatusInfo
 	report.Details = mt.msg
+	return &report
+}
+
+// NumberTracker is useful for tracking the latest number about something.
+type NumberTracker struct {
+	num float64
+	mu  sync.RWMutex
+}
+
+// Set sets the tracker.
+func (nt *NumberTracker) Set(num float64) {
+	nt.mu.Lock()
+	nt.num = num
+	nt.mu.Unlock()
+}
+
+// GetReport constructs and returns a report.
+func (nt *NumberTracker) GetReport(name string) *Report {
+	nt.mu.RLock()
+	defer nt.mu.RUnlock()
+	var report Report
+	report.Name = name
+	report.Status = StatusInfo
+	report.Details = strconv.FormatFloat(nt.num, 'f', -1, 64)
 	return &report
 }
