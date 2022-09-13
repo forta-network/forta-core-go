c17d499b7883bff319b4576a27ea03c1b81dc7fb
Peter Bwire <bwire517@gmail.com>
2022-09-13 09:43:46 +0300

    close the reader and writer always

--- utils/compress.go
+++ utils/compress.go
@@ -10,20 +10,20 @@ import (
 func GzipEncode(b []byte) ([]byte, error) {
 	var buf bytes.Buffer
 	w := gzip.NewWriter(&buf)
+	defer w.Close()
 	_, err := w.Write(b)
 	if err != nil {
 		return nil, err
 	}
-	defer w.Close()
 	return buf.Bytes(), nil
 }
 
 // GzipDecode decompresses bytes.
 func GzipDecode(b []byte) ([]byte, error) {
 	r, err := gzip.NewReader(bytes.NewBuffer(b))
+	defer r.Close()
 	if err != nil {
 		return nil, err
 	}
-	defer r.Close()
 	return io.ReadAll(r)
 }
