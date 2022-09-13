501a59aa7a4f1ea0732be3fbb4f7da7f887a1ad5
Peter Bwire <bwire517@gmail.com>
2022-09-13 09:49:13 +0300

    check error before deffering for reader

--- utils/compress.go
+++ utils/compress.go
@@ -21,9 +21,9 @@ func GzipEncode(b []byte) ([]byte, error) {
 // GzipDecode decompresses bytes.
 func GzipDecode(b []byte) ([]byte, error) {
 	r, err := gzip.NewReader(bytes.NewBuffer(b))
-	defer r.Close()
 	if err != nil {
 		return nil, err
 	}
+	defer r.Close()
 	return io.ReadAll(r)
 }
