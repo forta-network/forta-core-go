131ad789bac308425c5cacc992d8b6c9257dace4
ali <aliomerkeser@gmail.com>
2022-10-26 12:03:25 +0300

    add combiner feed range test

--- feeds/combiner_test.go
+++ feeds/combiner_test.go
@@ -2,61 +2,43 @@ package feeds
 
 import (
 	"context"
-	"sync"
 	"testing"
-
-	"github.com/forta-network/forta-core-go/clients/health"
-	"github.com/forta-network/forta-core-go/utils"
+	"time"
 )
 
 func Test_combinerFeed_StartRange(t *testing.T) {
-	type fields struct {
-		start            uint64
-		end              uint64
-		offset           int
-		ctx              context.Context
-		cache            utils.Cache
-		started          bool
-		rateLimit        *time.Ticker
-		lastAlert        health.MessageTracker
-		alertCh          chan *domain.AlertEvent
-		client           *graphql.Client
-		botSubscriptions map[string][]string
-		botsMu           sync.RWMutex
-		alertCache       utils.Cache
-		handlers         []cfHandler
-	}
 	type args struct {
 		start uint64
 		end   uint64
 		rate  int64
 	}
 	tests := []struct {
-		name   string
-		fields fields
-		args   args
+		name string
+		args args
 	}{
-		// TODO: Add test cases.
+		{
+			name: "successfully feeds range",
+			args: args{
+				start: 1666766210406,
+				end:   uint64(1666766210406 + time.Minute.Milliseconds()),
+				rate:  0,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(
 			tt.name, func(t *testing.T) {
-				cf := &combinerFeed{
-					start:            tt.fields.start,
-					end:              tt.fields.end,
-					offset:           tt.fields.offset,
-					ctx:              tt.fields.ctx,
-					cache:            tt.fields.cache,
-					started:          tt.fields.started,
-					rateLimit:        tt.fields.rateLimit,
-					lastAlert:        tt.fields.lastAlert,
-					alertCh:          tt.fields.alertCh,
-					client:           tt.fields.client,
-					botSubscriptions: tt.fields.botSubscriptions,
-					botsMu:           tt.fields.botsMu,
-					alertCache:       tt.fields.alertCache,
-					handlers:         tt.fields.handlers,
+				ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+				defer cancel()
+				cf, err := NewCombinerFeed(
+					ctx, CombinerFeedConfig{
+						APIUrl: "https://api.forta.network/graphql",
+					},
+				)
+				if err != nil {
+					t.Fatal(err)
 				}
+				cf.AddSubscription("0x5e13c2f3a97c292695b598090056ba5d52f9dcc7790bcdaa8b6cd87c1a1ebc0f", "0xlocalbot")
 				cf.StartRange(tt.args.start, tt.args.end, tt.args.rate)
 			},
 		)
