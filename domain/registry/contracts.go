2779908df2855cd0016f2f7e053772924b3a749e
Caner Çıdam <canercidam01@gmail.com>
2022-11-24 16:19:56 +0300

    let ens store be able to use a resolver func

--- domain/registry/contracts.go
+++ domain/registry/contracts.go
@@ -3,11 +3,13 @@ package registry
 import "github.com/ethereum/go-ethereum/common"
 
 type RegistryContracts struct {
-	Dispatch            common.Address
-	AgentRegistry       common.Address
-	ScannerRegistry     common.Address
+	Dispatch           common.Address
+	AgentRegistry      common.Address
+	ScannerRegistry    common.Address
+	ScannerNodeVersion common.Address
+	FortaStaking       common.Address
+	Forta              common.Address
+
 	ScannerPoolRegistry common.Address
-	ScannerNodeVersion  common.Address
-	FortaStaking        common.Address
-	Forta               common.Address
+	StakeAllocator      common.Address
 }
