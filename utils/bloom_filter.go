4d3e0374509261f4a211375e11532a177eed2843
ali <aliomerkeser@gmail.com>
2023-02-28 12:41:15 +0300

    add helper method for bloom filter

---
+++ utils/bloom_filter.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"bytes"
+	"encoding/base64"
+
+	"github.com/bits-and-blooms/bloom"
+	"github.com/forta-network/forta-core-go/protocol"
+)
+
+func RecreateBloomFilter(bf *protocol.BloomFilter) (*bloom.BloomFilter, error) {
+	b, err := base64.StdEncoding.DecodeString(bf.Bitset)
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := HexToBigInt(bf.M)
+	if err != nil {
+		return nil, err
+	}
+
+	k, err := HexToBigInt(bf.K)
+	if err != nil {
+		return nil, err
+	}
+
+	bFilter := bloom.New(uint(m.Uint64()), uint(k.Uint64()))
+	if _, err := bFilter.ReadFrom(bytes.NewBuffer(b)); err != nil {
+		return nil, err
+	}
+
+	return bFilter, nil
+}
+
