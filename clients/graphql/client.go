98225e005c9b0f38d7efc4f7d347109b03988a25
ali <aliomerkeser@gmail.com>
2023-03-10 10:11:58 +0300

    add label metadata to handle alert events

--- clients/graphql/client.go
+++ clients/graphql/client.go
@@ -115,6 +115,7 @@ func AlertToProto(alert *getAlertsAlertsAlertsResponseAlertsAlert) *protocol.Ale
 			Entity:     label.Entity,
 			EntityType: label.EntityType,
 			Remove:     label.Remove,
+			Metadata: label.Metadata,
 		}
 	}
 
