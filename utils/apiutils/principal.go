3a118836f862e132b7b3db8904bf88011b15591c
Caner Çıdam <canercidam01@gmail.com>
2023-12-20 14:01:53 +0300

    add utility methods to get and set bot id in context

--- utils/apiutils/principal.go
+++ utils/apiutils/principal.go
@@ -7,6 +7,7 @@ type contextKey int
 const (
 	contextKeyPrincipalAddressValue contextKey = 1
 	contextKeyBatchValue            contextKey = 2
+	contextKeyBotID                 contextKey = 3
 )
 
 func GetAddress(ctx context.Context) string {
@@ -23,4 +24,12 @@ func GetBatch(ctx context.Context) string {
 
 func SetBatch(ctx context.Context, batch string) context.Context {
 	return context.WithValue(ctx, contextKeyBatchValue, batch)
+}
+
+func GetBotID(ctx context.Context) string {
+	return ctx.Value(contextKeyBotID).(string)
+}
+
+func SetBotID(ctx context.Context, addr string) context.Context {
+	return context.WithValue(ctx, contextKeyBotID, addr)
 }
